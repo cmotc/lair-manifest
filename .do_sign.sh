9dc7387180e7f98278c335f4baa0ffca9f864d84
idk <problemsolver@openmailbox.org>
2017-04-06 02:05:34 -0400

    'cmotc'  170406020534m

--- .do_sign.sh
+++ .do_sign.sh
@@ -8,6 +8,6 @@ for item in $(ls *.deb); do debsigs --default-key $KEY $item; done
 for item in $(ls *.tar.xz); do gpg --batch --yes --detach-sign -u $KEY $item; done
 for item in $(ls *.tar.gz); do gpg --batch --yes --detach-sign -u $KEY $item; done
 cd lair-msi
-gpg --batch --yes --detach-sign -u $KEY lair-msi/lair-x86.msi
-gpg --batch --yes --detach-sign -u $KEY lair-msi/lair-x64.msi
+gpg --batch --yes --detach-sign -u $KEY lair-x86.msi
+gpg --batch --yes --detach-sign -u $KEY lair-x64.msi
 cd $s
