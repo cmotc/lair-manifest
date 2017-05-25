58e361b330d55104147b6319c95239ad4d94373d
idk <problemsolver@openmailbox.org>
2017-05-25 16:50:10 -0400

    'cmotc'  170525165010m

--- .fix_lua.sh
+++ .fix_lua.sh
@@ -1,1 +1,13 @@
-for item in $(ls /usr/include/lua5.2/); do sudo ln -s /usr/include/lua5.2/$item /usr/include/$item; done
+
+for item in $(ls /usr/include/lua5.1/); do
+        sudo ln -v -f -s /usr/include/lua5.1/$item /usr/include/$item;
+done
+
+#for item in $(ls /usr/include/lua5.2/); do
+#        sudo ln -s /usr/include/lua5.2/$item /usr/include/$item;
+#done
+
+#for item in $(ls /usr/include/lua5.3/); do
+#        sudo ln -s /usr/include/lua5.3/$item /usr/include/$item;
+#done
+
