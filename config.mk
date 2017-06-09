443285ceca3406ff14a368d300b24558ed1a45b0
[B <idk@fireaxe.local>
2017-06-09 15:21:39 -0400

    'cmotc' 'Right now map scripting is broken because I am making the variables that describe the maps ongoing state make more sense' 170609152139m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Now AutoRect should be able to tell what quadrant a collision happens in, which will make adjusting the sprite positions easier'
+DEV_MESSAGE = 'Right now map scripting is broken because I am making the variables that describe the maps ongoing state make more sense'
