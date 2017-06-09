56d57aa86f96134a36d6a9c441650935bb833470
[B <idk@fireaxe.local>
2017-06-08 23:28:16 -0400

    'cmotc' 'Moving to AutoRect class, which is going to replace rect for the purposes of the game' 170608232816m

--- config.mk
+++ config.mk
@@ -1,3 +1,5 @@
 GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
+
+DEV_MESSAGE = 'Moving to AutoRect class, which is going to replace rect for the purposes of the game'
