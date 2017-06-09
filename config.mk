55112a54465ef932f9a458712243f0e6a2fa6e0b
[B <idk@fireaxe.local>
2017-06-09 01:43:35 -0400

    'cmotc' 'removing the old crappy version of the collision detection and replacing it with the autorect version' 170609014335m

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
-VERSION = '0.9'
+VERSION = '0.91'
 
-DEV_MESSAGE = 'OK, now the only time we need a raw rectangle to create an AutoRect or call the draw functions'
+DEV_MESSAGE = 'removing the old crappy version of the collision detection and replacing it with the autorect version'
