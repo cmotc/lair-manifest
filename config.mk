bf746acf216600e92e2a1a06fd34c2a82fa44bf0
[B <idk@fireaxe.local>
2017-06-09 02:34:17 -0400

    'cmotc' 'Now AutoRect should be able to tell what quadrant a collision happens in, which will make adjusting the sprite positions easier' 170609023417m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'removing the old crappy version of the collision detection and replacing it with the autorect version'
+DEV_MESSAGE = 'Now AutoRect should be able to tell what quadrant a collision happens in, which will make adjusting the sprite positions easier'
