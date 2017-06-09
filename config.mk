548af589d29d21de305e5633ebf213a85f3b6d0a
[B <idk@fireaxe.local>
2017-06-09 00:21:49 -0400

    'cmotc' 'OK, now the only time we need a raw rectangle to create an AutoRect or call the draw functions' 170609002149m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'More progess on moving off of raw Video.Rect. After this I am going to create a convenience library for using vala and sdl to create 2D games'
+DEV_MESSAGE = 'OK, now the only time we need a raw rectangle to create an AutoRect or call the draw functions'
