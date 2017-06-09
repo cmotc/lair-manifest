9dceceb6bab550304e24ced5ab31889f154a377c
[B <idk@fireaxe.local>
2017-06-08 23:52:13 -0400

    'cmotc' 'More progess on moving off of raw Video.Rect. After this I am going to create a convenience library for using vala and sdl to create 2D games' 170608235213m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Moving to AutoRect class, which is going to replace rect for the purposes of the game'
+DEV_MESSAGE = 'More progess on moving off of raw Video.Rect. After this I am going to create a convenience library for using vala and sdl to create 2D games'
