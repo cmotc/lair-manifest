e7a49dd98fd1f05e207d167488f887c59af70c6a
[B <idk@fireaxe.local>
2017-06-09 01:46:05 -0400

    'cmotc' 'removing the old crappy version of the collision detection and replacing it with the autorect version' 170609014605m

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
-VERSION = '0.91'
+VERSION = '0.9'
 
 DEV_MESSAGE = 'removing the old crappy version of the collision detection and replacing it with the autorect version'
