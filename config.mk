61a13faab9ea23c7f84705988c2fce65fba3ad16
[B <idk@fireaxe.local>
2017-06-10 01:19:07 -0400

    'cmotc' 'starting to clean up the stuff that shares information with lua and make the map describing variables in Lua more sensible and self-explanatory' 170610011907m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Right now map scripting is broken because I am making the variables that describe the maps ongoing state make more sense'
+DEV_MESSAGE = 'starting to clean up the stuff that shares information with lua and make the map describing variables in Lua more sensible and self-explanatory'
