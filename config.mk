45a6ff1e5bbb4b57a577f1a580af602460080525
[B <idk@fireaxe.local>
2017-06-11 01:35:59 -0400

    'cmotc' 'Continued paring down the room class. Need to fix the AI script loader, then switch to quadrant-based collision detection.' 170611013559m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Fixed lua-scriptable map generation. Soon I will be able to go back to doing the fun stuff.'
+DEV_MESSAGE = 'Continued paring down the room class. Need to fix the AI script loader, then switch to quadrant-based collision detection.'
