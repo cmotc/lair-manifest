54d9677d4ecc24fe98e79c5ddcefc76013294e6a
[B <idk@fireaxe.local>
2017-06-14 10:46:17 -0400

    'cmotc' 'Preparing to control log verbosity. Still have not figured out why mobs are invisible. Removed Redundant Logging.' 170614104617m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Improved collision detection. Need to control log verbosity soon. Also need to figure out what is up with mobs, they are being inserted but not drawn, but they have positions, which are just AutoRects and not necessarily attached to a real texture, which means that it is possible to collide with an invisible entity. Bug or feature? Whatever.'
+DEV_MESSAGE = 'Preparing to control log verbosity. Still have not figured out why mobs are invisible. Removed Redundant Logging.'
