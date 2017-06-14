4dafa05b195b96c7b0321cf1b98f36a41e670cf4
[B <idk@fireaxe.local>
2017-06-14 14:53:23 -0400

    'cmotc' 'Fixed invisible mobs. Need to re-design the dungeon generator script to account for overlaps.' 170614145323m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Framerate adjustment. Removed inefficient and redundant logs'
+DEV_MESSAGE = 'Fixed invisible mobs. Need to re-design the dungeon generator script to account for overlaps.'
