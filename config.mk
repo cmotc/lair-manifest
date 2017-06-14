55ba284ebb2ca67ff54151e9823ea93b804bc9f1
[B <idk@fireaxe.local>
2017-06-14 11:37:43 -0400

    'cmotc' 'Framerate adjustment. Removed inefficient and redundant logs' 170614113743m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Preparing to control log verbosity. Still have not figured out why mobs are invisible. Removed Redundant Logging.'
+DEV_MESSAGE = 'Framerate adjustment. Removed inefficient and redundant logs'
