eb45f2ea360656bdcb7fed0d69fcbeda3471c480
[B <idk@fireaxe.local>
2017-06-14 00:04:04 -0400

    'cmotc' 'Improved collision detection. Need to control log verbosity soon. Also need to figure out what is up with mobs, they are being inserted but not drawn, but they have positions, which are just AutoRects and not necessarily attached to a real texture, which means that it is possible to collide with an invisible entity. Bug or feature? Whatever.' 170614000403m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Continued paring down the room class. Need to fix the AI script loader, then switch to quadrant-based collision detection.'
+DEV_MESSAGE = 'Improved collision detection. Need to control log verbosity soon. Also need to figure out what is up with mobs, they are being inserted but not drawn, but they have positions, which are just AutoRects and not necessarily attached to a real texture, which means that it is possible to collide with an invisible entity. Bug or feature? Whatever.'
