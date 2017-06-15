992c926ac92b351510b7b3363ab65d72be247319
[B <idk@fireaxe.local>
2017-06-15 17:52:02 -0400

    'cmotc' 'Welp. All the outstanding problems are fixed, but now there is a new one. Map generation takes 25 minutes on the target platform. Holy hell.' 170615175202m

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@ GH_NAME = 'cmotc'
 KEY = 'C0CEEE297B5FE45FF610AAC6F05F85FA446C042B'
 VERSION = '0.9'
 
-DEV_MESSAGE = 'Fixed invisible mobs. Need to re-design the dungeon generator script to account for overlaps.'
+DEV_MESSAGE = 'Welp. All the outstanding problems are fixed, but now there is a new one. Map generation takes 25 minutes on the target platform. Holy hell.'
